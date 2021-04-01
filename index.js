bcaee39f6a21a8f0d85c8bf57354e39a7ca7c2b9
Ola <32633868+ola2703@users.noreply.github.com>
2021-04-01 20:19:16 +0200

    Add fetching movie data from movie API.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=fc5d2d36", {
+        params:{
+            s: 'avengers'
+        }
+    });
+    console.log(response.data);
+}
+
+fetchData();
